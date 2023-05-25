800b043020cff5e87c5d104ae121049805ad12d8
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2023-05-25 12:02:21 +0700

    update1

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,6 +1,9 @@
 <x-layout title="Detail">
     <div class="d-flex">
-      <i class="bi bi-caret-right-fill" style="font-size: 3rem; color: rgba(40, 84, 48, 1)"></i>
-      <p class="fs-1 fw-bold pt-2" style="rgba(40, 84, 48, 1)">REGISTER FOR {{ $event->event_name }}</p>
+      <img src="/storage/images/{{ $event->picture }}" alt="EVENT" style="height: 100vh; width: 50vw">
+      <div class="d-flex pt-3 ps-4">
+        <i class="bi bi-caret-right-fill" style="font-size: 3rem; color: rgba(40, 84, 48, 1)"></i>
+        <p class="fs-1 fw-bold pt-2 ps-4" style="color: rgba(40, 84, 48, 1)">Details</p>
+      </div>
     </div>
 </x-layout>
