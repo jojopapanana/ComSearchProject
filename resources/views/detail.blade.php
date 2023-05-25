faaacc8f58595ecfbfe22002ed6542159622519d
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2023-05-25 11:13:57 +0700

    update

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Detail">
+    <div class="d-flex">
+      <i class="bi bi-caret-right-fill" style="font-size: 3rem; color: rgba(40, 84, 48, 1)"></i>
+      <p class="fs-1 fw-bold pt-2" style="rgba(40, 84, 48, 1)">REGISTER FOR {{ $event->event_name }}</p>
+    </div>
+</x-layout>
